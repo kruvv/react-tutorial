1ce7076b9efb6e694ddc7aa68e68b6cb01cfb235
viktor <kruvv@mail.ru>
2019-02-19 19:18:41 +0300

    Add 14_portals

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import './index.css';
 //import  Lesson  from './09_fragment_&_css/Lesson';
 //import  Lesson  from './10_forms/Lesson';
 //import  Lesson  from './12_refs/Lesson';
-import  Lesson  from './13_high_order_components/Lesson';
+//import  Lesson  from './13_high_order_components/Lesson';
+import  Lesson  from './14_portals/Lesson';
 import * as serviceWorker from './serviceWorker';
 
 //Создание компонентов реакт без файла с расширением .jsx
